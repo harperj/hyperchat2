f58b8a5468d50ac019e905add798f0f4f4103afe
elsehow <yes@cosmopol.is>
2016-03-01 15:31:52 -0800

    updated to include hyperx(ify) and virtual-dom

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hello')
+// set up dom stuff
+var vdom   = require('virtual-dom')
+, hyperx   = require('hyperx')
+, hx       = hyperx(vdom.h)
+, main     = require('main-loop')
+, loop     = main({ times: 0 }, render, vdom)
+document.querySelector('#app').appendChild(loop.target)
+
+function render (state) {
+    return hx`<div>
+        <h1>clicked ${state.times} times</h1>
+        <button onclick=${onclick}>click me!</button>
+        </div>`
+
+    function onclick () {
+        loop.update({ times: state.times + 1 })
+    }
+}
+
+console.log('run')
